2ab042669944f6098eccff8c700d07d3d871ea69
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-12-05 20:12:58 +0530

    Updated the products, added ProductScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,11 +6,11 @@ import products from '../products'
 const HomeScreen = () => {
     return (
         <>
-            <h1>Latest Products</h1>
+            <h1>Latest Courses</h1>
             <Row>
                 {products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product key={product._id} product={product} />
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
                     </Col>
                 ))}
             </Row>
